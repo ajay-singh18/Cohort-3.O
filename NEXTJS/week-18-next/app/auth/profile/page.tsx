4761ab15e5acae59a68b34c9f86c195f6bfa2205
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-08-30 01:17:21 +0530

    layouts

---
+++ NEXTJS/week-18-next/app/auth/profile/page.tsx
@@ -0,0 +1,5 @@
+export default function Profile(){
+    return (
+        <h1>hello from profile</h1>
+    )
+}
---
+++ NEXTJS/week-18-next/app/components/Navbar.tsx
@@ -0,0 +1,5 @@
+export default function Navbar(){
+    return (
+        <div className="border-b text-white">Header</div>
+    )
+}
