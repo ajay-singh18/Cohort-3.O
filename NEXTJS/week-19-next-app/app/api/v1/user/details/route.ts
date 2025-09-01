a10d72bc1070a529c7891dd65d9e6fd47516ecca
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-09-02 03:50:40 +0530

    next js api

---
+++ NEXTJS/week-19-next-app/app/api/v1/user/details/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export function GET(){
+    return NextResponse.json({
+        user:"ajay",
+        email: "sfksfjklsfjsdklfdjslf"
+    })
+}
+export function POST(){
+    return NextResponse.json({
+        user:"ajay",
+        email: "sfksfjklsfjsdklfdjslf"
+    })
+}
+export function PUT(){
+    return NextResponse.json({
+        user:"ajay",
+        email: "sfksfjklsfjsdklfdjslf"
+    })
+}
+
