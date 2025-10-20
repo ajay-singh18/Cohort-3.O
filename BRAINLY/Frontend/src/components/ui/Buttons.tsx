459103ab1a28e38d16bf60322b8b8327f73edc16
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-10-20 23:58:26 +0530

    changes

--- BRAINLY/Frontend/src/components/ui/Buttons.tsx
+++ BRAINLY/Frontend/src/components/ui/Buttons.tsx
@@ -17,7 +17,7 @@ const sizeStyle={
   "lg": " py-3 px-6"
 }
 const defaultStyle = "rounded-md "
-import React from 'react'   //
+import React from 'react'   // fhjhghjhghgghkg
 
 const Buttons = (props:ButtonProps) => {
   return (
