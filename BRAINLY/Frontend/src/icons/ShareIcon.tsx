ad5a4a356b020afbc35c251385771b9e0e8d5351
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-10-26 23:58:03 +0530

    postgress

--- BRAINLY/Frontend/src/icons/ShareIcon.tsx
+++ BRAINLY/Frontend/src/icons/ShareIcon.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { sizeVariants } from './index'
 
 function ShareIcon(props) {
---
+++ WEEK-18 Postgress/src/index.ts
@@ -0,0 +1,34 @@
+import {Client} from "pg"
+import express from "express"
+// const pgClient = new Client({ // can directly paste link also
+//     user: "neondb_owner",
+//     password : "npg_pLtSCYhz86xj",
+//     port : 5432,
+//     host: "ep-autumn-butterfly-ahb1ae52-pooler.c-3.us-east-1.aws.neon.tech",
+//     database:"neondb",
+//     ssl: true
+// })
+
+const pgClient = new Client("postgresql://neondb_owner:npg_pLtSCYhz86xj@ep-autumn-butterfly-ahb1ae52-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+
+pgClient.connect();
+const app = express();
+app.use(express.json())
+
+app.post("/signup",async (req,res)=>{
+   try{
+     const {username,password,email} = req.body
+    const insertQuery = `INSERT INTO users (username,email, password) VALUES($1,$2,$3);`
+    const response =  await pgClient.query(insertQuery,[username,email,password])
+    res.json({
+        message: "Signed up successfully"
+    })
+   }catch(e){
+     console.log(e);
+     res.json({
+        e
+     })
+   }
+
+})
+app.listen(3000)
