9b94fd5ba5191cd3cde7c125e21bdf22ad661df0
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-10-31 19:42:02 +0530

    dynamic rote

--- WEEK-20 NXTJS/week-20-next-app/app/blogs/[blogId]/page.tsx
+++ WEEK-20 NXTJS/week-20-next-app/app/blogs/[blogId]/page.tsx
@@ -1,7 +1,20 @@
-export default function Blogs(){
+import axios from "axios";
+
+//Dynamic routes
+export default async function Blogs({params}:any){
+    const blogId = (await params).blogId;
+    console.log(blogId)
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${blogId}`)
+    const data = response.data
+    console.log(data);
+    
     return (
         <div>
-            blogs
+            blogs {blogId}
+            <br />
+            {data.title}
+            <br />
+            {data.id}
         </div>
     )
 }
---
+++ WEEK-20 NXTJS/week-20-next-app/app/todos/[...todoId]/page.tsx
@@ -0,0 +1,6 @@
+export default async function Todo({params}:any){
+    const todoId = (await params).todoId
+    return <div>
+        {JSON.stringify(todoId) }
+    </div>
+} 
