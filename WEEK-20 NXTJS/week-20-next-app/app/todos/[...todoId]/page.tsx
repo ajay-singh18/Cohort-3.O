9597a639861f496266faf5ed67b796e32343345a
Ajay Kumar <ajaykrsingh8709@gmail.com>
2025-11-01 23:10:06 +0530

    slugs

--- WEEK-20 NXTJS/week-20-next-app/app/todos/[...todoId]/page.tsx
+++ WEEK-20 NXTJS/week-20-next-app/app/todos/[...todoId]/page.tsx
@@ -1,5 +1,5 @@
 export default async function Todo({params}:any){
-    const todoId = (await params).todoId
+    const todoId = (await params).todoId // params is a promise so we have to await it
     return <div>
         {JSON.stringify(todoId) }
     </div>
